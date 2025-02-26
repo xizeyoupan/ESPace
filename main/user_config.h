a090ab6a2d1cc8ec357d59e798f26adf63a06d40
我葱 <12865910+helihan@user.noreply.gitee.com>
2025-02-26 09:42:43 +0800

    fix:typo

--- main/user_config.h
+++ main/user_config.h
@@ -48,7 +48,8 @@
 #include "nvs_util.h"
 #include "tftest.h"
 
-typedef enum {
+typedef enum
+{
     COLOR_RED = 0xff0000,
     COLOR_GREEN = 0x00ff00,
     COLOR_BLUE = 0x0000ff,
@@ -64,31 +65,33 @@ typedef enum {
 #define USER_CONFIG_NVS_KEY "config_data"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-typedef enum {
-    COMMAND_MODEL = 0,
-    COMMAND_MODEL = 1,
-} model_type;
+    typedef enum
+    {
+        COMMAND_MODEL = 0,
+        CONTIOUS_MODEL = 1,
+    } model_type;
 
-typedef struct
-{
-    model_type type;
-    uint16_t sample_tick;
-    uint16_t sample_size;
-    uint8_t* data_p;
-} model_t;
+    typedef struct
+    {
+        model_type type;
+        uint16_t sample_tick;
+        uint16_t sample_size;
+        uint8_t *data_p;
+    } model_t;
 
 #pragma pack(1)
-typedef struct
-{
-    gpio_num_t ws2812_gpio_num;
-    gpio_num_t mpu_sda_gpio_num;
-    gpio_num_t mpu_scl_gpio_num;
-    uint8_t enable_imu_det;
-    uint8_t enable_ws_log;
-} user_config_t;
+    typedef struct
+    {
+        gpio_num_t ws2812_gpio_num;
+        gpio_num_t mpu_sda_gpio_num;
+        gpio_num_t mpu_scl_gpio_num;
+        uint8_t enable_imu_det;
+        uint8_t enable_ws_log;
+    } user_config_t;
 
 #ifdef __cplusplus
 }
